3a04018851e3699a8c8a2cc614e65e812d177771
CalvinHoMicrochip <calvin.ho@microchip.com>
2024-11-22 10:17:49 +0800

    Add files via upload

---
+++ PIC18F57Q43/IDE620_FactoryFW.X/build/default/debug/OLED128x64.p1.d
@@ -0,0 +1,21 @@
+build/default/debug/OLED128x64.p1:  \
+OLED128x64.c  \
+OLED128x64.h  \
+mcc_generated_files/system/system.h  \
+mcc_generated_files/system/../system/config_bits.h  \
+mcc_generated_files/system/../system/clock.h  \
+mcc_generated_files/system/../system/pins.h  \
+mcc_generated_files/system/../adc/adcc.h  \
+mcc_generated_files/system/../clc/clc1.h  \
+mcc_generated_files/system/../i2c_host/i2c1.h  \
+mcc_generated_files/system/../i2c_host/i2c_host_event_types.h  \
+mcc_generated_files/system/../i2c_host/i2c_host_types.h  \
+mcc_generated_files/system/../i2c_host/i2c_host_interface.h  \
+mcc_generated_files/system/../pwm/ccp1.h  \
+mcc_generated_files/system/../spi/spi1.h  \
+mcc_generated_files/system/../spi/spi_interface.h  \
+mcc_generated_files/system/../timer/tmr2.h  \
+mcc_generated_files/system/../timer/timer_interface.h  \
+mcc_generated_files/system/../system/interrupt.h  \
+mcc_generated_files/timer/delay.h  \
+OLED_FONTs.c 
